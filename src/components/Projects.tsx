b2324f39f21bff1db1fde87db748dcb0369106a0
rustadadam <rustadadam@gmail.com>
2025-05-21 13:40:07 -0600

    adjusted transition

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -134,10 +134,11 @@ const Projects: React.FC = () => {
             <motion.div
               key={project.id}
               initial={{ opacity: 0, x: 50 }}
-              animate={inView ? { opacity: 1, x: 0 } : {}}
-              transition={{ duration: 0.5, delay: index * 0.1 }}
+              animate={inView ? { opacity: 1, x: 0, transition: { duration: 1.0, ease: 'easeOut', delay: index * 0.23 } } : {}}
+              exit={{ opacity: 0, x: 50, transition: { duration: 2, ease: 'easeIn' } }}
               className="flex-none w-[430px] h-[600px] bg-white dark:bg-gray-900 rounded-3xl overflow-hidden border-0 shadow-[0_10px_20px_0_rgba(9,189,255,0.13),0_1.5px_16px_0_rgba(0,0,0,0.06)] dark:shadow-[0_10px_24px_0_rgba(9,189,255,0.18),0_6px_26px_0_rgba(0,0,0,0.10)] transition duration-300 flex flex-col justify-between"
               style={{ fontFamily: 'Inter, Poppins, sans-serif' }}
+              layout
             >
               <div className="h-64 overflow-hidden rounded-t-3xl flex items-center justify-center bg-gray-100 dark:bg-gray-800">
                 <img
